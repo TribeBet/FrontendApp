3147a3887aecff02b99d7dabc913e03e57dd2181
VishalPachpor <Vishalpatil080502@gmail.com>
2023-12-20 18:18:52 +0530

    Components added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import styles from "../styles/Home.module.css";
-import InstructionsComponent from "../components/InstructionsComponent";
 import { useRouter } from "next/router";
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
     <div>
       <main className={styles.main}>
         <button className="bg-white" onClick={() => { router.push('/bets') }} > Hello World </button>
-        <InstructionsComponent></InstructionsComponent>
+
       </main>
     </div>
   );
