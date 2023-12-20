affb006648743cf8d84a91d12152a943c2b8035d
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2023-12-20 18:30:39 +0530

    created home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,27 @@
 import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
+// import Search from "@/components/Search";
+import CardSlider from "@/components/Card/CardSlider";
+import Versus from "@/components/VersusCard/Versus";
 
 export default function Home() {
   const router = useRouter();
   return (
     <div>
       <main className={styles.main}>
-        <button className="bg-white" onClick={() => { router.push('/bets') }} > Hello World </button>
+        {/* <button className="bg-white" onClick={() => { router.push('/bets') }} > Hello World </button> */}
+        <div className="mx-5">
+          <div className="relative pt-20">
+            <CardSlider />
+          </div>
+          <h1>"Bet Smarter, Win Bigger with tribebet Betting!" </h1>
+          <div className=" absoulte pt-4">
+          </div>
+          <div className="relative pb-2">
+            <Versus />
+          </div>
+
+        </div>
 
       </main>
     </div>
