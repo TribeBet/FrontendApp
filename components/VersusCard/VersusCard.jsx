e6ee5b7e86e0acf2a2f4ce884ec79970323817a7
Harshal Bhangale <buddyharshal2751@gmail.com>
2023-12-22 11:10:51 +0530

    changed bet component bg

--- components/VersusCard/VersusCard.jsx
+++ components/VersusCard/VersusCard.jsx
@@ -1,8 +1,7 @@
-"use client"
 // components/VersusCard.js
 import React, { useState } from 'react';
 import BettingSlip from './BettingSlip/BettingSlip';
-
+import gameData from '../../lib/gameData';
 const VersusCard = ({ image1, image2, text1, text2, team1, team2, prob1, prob2, prob3, date, sportName, tournament }) => {
   const [bettingSlipVisible, setBettingSlipVisible] = useState(false);
 
@@ -11,44 +10,44 @@ const VersusCard = ({ image1, image2, text1, text2, team1, team2, prob1, prob2, 
   };
 
   return (
-    <div className="bg-base-100 shadow-xl p-4 mb-4 text-center max-w-md mx-auto rounded-xl">
+    <div className="bg-gradient-to-r from-gray-700 to-gray-900 shadow-xl p-4 mb-4 text-center max-w-md mx-auto rounded-xl">
       <div className="mb-4 flex items-center justify-between">
         <div className="text-left">
-          <h2 className="text-xl font-bold">{tournament} </h2>
+        <h2 className="text-l font-bold text-white">{tournament}</h2>
         </div>
         <div className="text-right">
-          <p className="text-gray-500">{date}</p>
+          <p className="text-gray-400">{date}</p>
         </div>
       </div>
       <div className="flex items-center justify-center mb-4">
         <div className="flex flex-col items-center mr-4">
           <img src={image1} alt="Team 1" className="w-24 h-auto rounded-xl mb-2" />
-          <p className="text-lg font-bold">{text1}</p>
+          <p className="text-lg font-bold text-white">{text1}</p>
         </div>
-        <span className="text-2xl font-bold mx-4">vs</span>
+        <span className="text-2xl font-bold mx-4 text-white">vs</span>
         <div className="flex flex-col items-center ml-4">
           <img src={image2} alt="Team 2" className="w-24 h-auto rounded-xl mb-2" />
-          <p className="text-lg font-bold">{text2}</p>
+          <p className="text-lg font-bold text-white">{text2}</p>
         </div>
       </div>
       <div className="flex justify-between gap-2">
         <button
           onClick={handleOpenBettingSlip}
-          className="result-button bg-transparent border-solid text-start border-2 border-gray-200 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:bg-gray-200 hover:scale-105"
+          className="result-button bg-black text-white border-solid text-start border-2 border-gray-300 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:border-purple-400 hover:scale-105"
         >
           {team1} Wins
           <span className="text-purple-400"> {prob1}</span>
         </button>
         <button
           onClick={handleOpenBettingSlip}
-          className="result-button bg-transparent border-solid text-start border-2 border-gray-200 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:bg-gray-200 hover:scale-105"
+          className="result-button bg-black text-white border-solid text-start border-2 border-gray-300 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:border-purple-400 hover:scale-105"
         >
           Draw
           <span className="text-purple-400"> {prob2}</span>
         </button>
         <button
           onClick={handleOpenBettingSlip}
-          className="result-button bg-transparent border-solid text-start border-2 border-gray-200 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:bg-gray-200 hover:scale-105"
+          className="result-button bg-black text-white border-solid text-start border-2 border-gray-300 w-13 items-center justify-center p-2 rounded-xl transition duration-300 ease-in-out transform hover:border-purple-400 hover:scale-105"
         >
           {team2} Wins
           <span className="text-purple-400"> {prob3}</span>
@@ -66,4 +65,3 @@ const VersusCard = ({ image1, image2, text1, text2, team1, team2, prob1, prob2, 
 };
 
 export default VersusCard;
-
